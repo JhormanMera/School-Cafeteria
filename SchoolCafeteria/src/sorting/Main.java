7f1f0bfd97cf7cfbef9b7805521e1c1e01dcd595
JhormanMera <71386868+JhormanMera@users.noreply.github.com>
2021-02-25 14:00:36 -0500

    SchoolCafeteria V2
    Average calculated and formatted correctly

--- SchoolCafeteria/src/sorting/Main.java
+++ SchoolCafeteria/src/sorting/Main.java
@@ -1,67 +1,68 @@
 package sorting;
 
 import java.io.BufferedReader;
-//import java.io.BufferedWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
+import java.io.OutputStreamWriter;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class Main {
-		
+
 	private static Double [] array;
 	private  static int setAmount;
 	private static BufferedReader br;
-	//private static BufferedWriter bw;
+	private static BufferedWriter bw;
 
+	public static String bubbleSort() {
+		int numbers =0;
+		int totalPositions =0;
 
-	
-	public static void bubbleSort() {
-		boolean changed = true;
-		for(int i=1; i<array.length && changed;i++) {
-			changed=false;
-			for(int k=1;k<array.length;k++) {
-				if(array[k-1]>array[k]) {
-					Double temp=array[k-1];
-					array[k-1]=array[k];
-					array[k]=temp;
-					changed= true;
+		for(int i=1; i<array.length;i++) {				
+			for(int k=0;k<array.length-i;k++) {
+				if(array[k]>array[k+1]) {
+					Double temp=array[k];
+					array[k]=array[k+1];
+					array[k+1]=temp;
+					totalPositions++;
 				}
 			}
-		}
+			numbers++;
+		}		
+		DecimalFormat df = new DecimalFormat("0.0#");
+		double average = (double)totalPositions/numbers;
+		average = Math.floor(average*100)/100;
+		String averageFormated = df.format(average).toString().replace(",", ".");		
+		String format= averageFormated+"-"+Arrays.toString(array).replace("[","").replace("]","").replace(",","");
+		return format;		
 	}
-		
-	public static void start() throws NumberFormatException, IOException {	
-		setAmount=Integer.parseInt(br.readLine());
-		
-		fullSets();
-	} 
-	
 	public static void fullSets() throws NumberFormatException, IOException {
-		int i=0;
+		int i=0;	
+
 		while (i<setAmount) {
 			String ages=br.readLine();
 			String[] arrayAges=ages.split(" ");
 			array=new Double[arrayAges.length];
 			for(int j=0; j<arrayAges.length; j++) {
-			array[j]=Double.parseDouble(arrayAges[j]);
-				
+				array[j]=Double.parseDouble(arrayAges[j]);				
 			}
 			i++;
-			bubbleSort();
-			System.out.println(Arrays.toString(array));
-			//bw.write(Arrays.toString(array));
-			
-		}
-		
-	}
-	
+			bw.write(bubbleSort()+"\n");
+			bw.flush();
+		}			
+	}	
+
+	public static void start() throws NumberFormatException, IOException {	
+		setAmount=Integer.parseInt(br.readLine());		
+		fullSets();
+	} 
+
+
 	public static void main(String[] args) throws NumberFormatException, IOException {		
 		br= new BufferedReader(new InputStreamReader(System.in));
-		//bw= new BufferedWriter(new OutputStreamWriter(System.out));
+		bw= new BufferedWriter(new OutputStreamWriter(System.out));
 		start();
 		br.close();	
-
 	}
-
 }
